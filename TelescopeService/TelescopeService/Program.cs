50c4fbd142270d874bbd8f95e6ac0c63f15ac3f0
unknown <gyori.reka@codespring.ro>
2023-10-21 02:48:44 +0300

    generate images with generator and upload to blob storage with dapr

---
+++ TelescopeService/TelescopeService/Program.cs
@@ -0,0 +1,28 @@
+﻿using Dapr.Client;
+using TelescopeService.Generator;
+
+const string blobStoreName = "blobstorage";
+const string create = "create";
+
+var daprClient = new DaprClientBuilder().Build();
+
+string response = Generator.Generate("green").Result;
+string downloadUrlFromGenerator = Generator.GetDownloadURL(response);
+
+var lastIndexOfSlash = downloadUrlFromGenerator.LastIndexOf("/");
+var imageName = downloadUrlFromGenerator.Substring(lastIndexOfSlash + 1, downloadUrlFromGenerator.Length - lastIndexOfSlash - 1);
+
+Console.WriteLine(downloadUrlFromGenerator);
+
+IReadOnlyDictionary<string, string> metaData = new Dictionary<string, string>()
+{
+    { "blobName", $"{imageName}" },
+};
+
+using (var client = new HttpClient())
+{
+    byte[] dataBytes = client.GetByteArrayAsync(downloadUrlFromGenerator).Result;
+    string encodedFileAsBase64 = Convert.ToBase64String(dataBytes);
+
+    await daprClient.InvokeBindingAsync(blobStoreName, create, encodedFileAsBase64, metaData);
+}
