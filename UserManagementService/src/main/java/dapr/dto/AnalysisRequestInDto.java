2f239b8f3ecece063c96c48e3b065444d7b53b1b
Balázs István-Lehel <balazs.lehel@codespring.ro>
2023-10-25 15:19:08 +0300

    * Removed unnecessary files * Removed parent pom.xml

--- UserManagementService/src/main/java/dapr/dto/AnalysisRequestInDto.java
+++ UserManagementService/src/main/java/dapr/dto/AnalysisRequestInDto.java
@@ -1,12 +1,10 @@
 package dapr.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 
 @Data
 @ToString
